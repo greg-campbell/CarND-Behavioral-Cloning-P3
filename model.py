8a4ec0f181c9cdf21c202c94e5496901a484a600
Greg Campbell <00gcampbell@gmail.com>
2017-02-06 16:08:12 +0000

    Generated a model.

--- model.py
+++ model.py
@@ -19,19 +19,24 @@ def draw(img, title = '', color = cv2.COLOR_BGR2RGB):
     display(f)
     plt.close(f)
 
+def read_image(path):
+    return cv2.imread('./data/' + path)
+
 # Read in the driving log CSV
 driving_log = pd.read_csv('./data/driving_log.csv')
 driving_log.head()
 
 test_image = cv2.imread('./data/' + driving_log['center'][0])
 
-draw(test_image, "Original image (center camera)")
+#draw(test_image, "Original image (center camera)")
 
 height, width, channels = test_image.shape
 print("Image dimensions:" , width, height)
 
 def crop_image(img, x, y, w, h):
-    return img[y:y+h, x:x+w]
+    y2 = y+h
+    x2 = x+w
+    return img[y:y2, x:x2]
 
 def resize_image(img, width, height):
     return cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
@@ -40,17 +45,7 @@ def preprocess(img):
     return resize_image(crop_image(img, 0, 50, width, 80), 200, 66)
 
 preprocessed = preprocess(test_image)
-draw(preprocessed, "After preprocessing (crop + resize)")
-
-def generate_batch_from_data(data, size = 64):
-    images = np.zeros((size, *preprocessed.shape))
-    steering_angles = np.zeros(size)
-    while 1:
-        for i in range(size):
-            row_num = np.random.randint(len(data))
-            images[i] = preprocess(data['center'][row_num])
-            steering_angles[i] = data['steering'][row_num]
-        yield images, steering_angles
+#draw(preprocessed, "After preprocessing (crop + resize)")
 
 from keras.models import Sequential
 from keras.layers.core import Activation, Dense, Dropout, Flatten
@@ -58,7 +53,7 @@ from keras.layers.core import Activation, Dense, Dropout, Flatten
 from keras.layers.convolutional import Convolution2D
 
 model = Sequential()
-model.add(Convolution2D(24, 5, 5, subsample=(2,2)))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2), input_shape=preprocessed.shape))
 model.add(Activation('relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2,2)))
 model.add(Activation('relu'))
@@ -83,8 +78,20 @@ model.add(Dense(16))
 model.add(Activation('relu'))
 model.add(Dense(1))
 
-from sklearn.model_selection import test_train_split
-data_train, data_val = test_train_split(driving_log)
+from sklearn.model_selection import train_test_split
+data_train, data_val = train_test_split(driving_log)
+
+def generate_batch_from_data(data, size = 32):
+    images = np.zeros((size, *preprocessed.shape))
+    steering_angles = np.zeros(size)
+    while 1:
+        for i in range(size):
+            row = data.iloc[[np.random.randint(len(data))]].reset_index()
+            images[i] = preprocess(read_image(row['center'][0]))
+            steering_angles[i] = row['steering'][0]
+        yield images, steering_angles
+
+
 
 model.compile('adam', 'mean_squared_error', ['mean_squared_error'])
 
