b3056dfb8633423c476650b29e6b68b6e2619a13
Greg Campbell <00gcampbell@gmail.com>
2017-02-19 03:46:33 +0000

    Model still sucks. Car crashes. =(

--- model.py
+++ model.py
@@ -6,24 +6,40 @@ import cv2
 ### Feel free to use as many code cells as needed.
 import matplotlib.pyplot as plt
 from IPython.display import display as display
-#from IPython.core.pylabtools import figsize, getfigs
+from IPython.core.pylabtools import figsize, getfigs
 #import math
 #import matplotlib.image as mpimg
 #import matplotlib.transforms as mtransforms
-def draw(img, title = '', color = cv2.COLOR_BGR2RGB):
+def draw(img, title = '', filename='', color = cv2.COLOR_BGR2RGB):
     color_img = cv2.cvtColor(img, color)
     """ Draw a single image with a title """
     f = plt.figure()
     plt.title(title)
     plt.imshow(color_img, cmap='gray')
-    display(f)
+    #display(f)
+    if filename != '':
+        plt.savefig('./img/'+filename+'.png')
     plt.close(f)
 
 def read_row(row):
   img = read_image(row['center'][0])
   angle = row['steering'][0]
+  if 1 == np.random.randint(2):
+      img = cv2.flip(img, 1)
+      angle = -angle
   return preprocess(img), angle
+
+def augment_row(row):
+  cameras = ['left', 'center', 'right']
+  offsets = [1, 0, -1]
+  camera_index = np.random.randint(3)
+  img = read_image(row[cameras[camera_index]][0].strip())
+  angle = row['steering'][0] + offsets[camera_index]*0.28
   
+  if 1 == np.random.randint(2):
+      img = cv2.flip(img, 1)
+      angle = -angle
+  return preprocess(img), angle
 
 def read_image(path):
     return cv2.imread('./data/' + path)
@@ -39,22 +55,19 @@ test_image = cv2.imread('./data/' + driving_log['center'][0])
 height, width, channels = test_image.shape
 print("Image dimensions:" , width, height)
 
-def crop_image(img, x, y, w, h):
-   return img[y:y+h,:] 
-#def crop_image(img, x, y, w, h):
-#    y2 = y+h
-#    x2 = x+w
-#    return img[y:y2, x:x2]
-#
+def crop_image(img, y1, y2):
+   #return img[y1:y2,:] 
+   return img
+
 def resize_image(img, width, height):
     return cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
 
 def preprocess(img):
-    return resize_image(crop_image(img, 0, 40, 320, 40), 200, 66 )
-#
+    return resize_image(crop_image(img, 53, 128), 32, 16 )
+
 preprocessed = preprocess(test_image)
 print(preprocessed.shape)
-#draw(preprocessed, "After preprocessing (crop + resize)")
+#draw(preprocessed, "After preprocessing (crop + resize)", "preprocessed")
 
 from keras.models import Sequential
 from keras.layers.core import Activation, Dense, Dropout, Flatten
@@ -67,17 +80,17 @@ model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=preprocessed.shape))
 #model.add(Cropping2D(cropping=((20, 10), (0, 0)), input_shape=preprocessed.shape))
 model.add(Convolution2D(24, 5, 5, subsample=(2,2)))
 model.add(Activation('relu'))
-model.add(Convolution2D(36, 5, 5, subsample=(2,2)))
+#model.add(Convolution2D(36, 5, 5, subsample=(2,2)))
 model.add(Activation('relu'))
-model.add(Convolution2D(48, 5, 5, subsample=(2,2)))
+model.add(Convolution2D(48, 3, 3))
 model.add(Dropout(0.5))
 model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
+#model.add(Convolution2D(64, 3, 3))
+#model.add(Activation('relu'))
+#model.add(Convolution2D(64, 3, 3))
+#model.add(Activation('relu'))
+#model.add(Dropout(0.5))
+#model.add(Activation('relu'))
 model.add(Flatten())
 model.add(Dense(1024))
 model.add(Dropout(0.5))
@@ -99,7 +112,11 @@ def generate_batch_from_data(data, size = 32):
     while 1:
         for i in range(size):
             row = data.iloc[[np.random.randint(len(data))]].reset_index()
-            x,y = read_row(row)
+            augment_var = False #np.random.rand() < 0.33
+            if augment_var:
+              x,y = augment_row(row)
+            else:
+              x,y = read_row(row)
             images[i] = x
             steering_angles[i] = y
         yield images, steering_angles
@@ -109,11 +126,10 @@ model.compile('adam', 'mean_squared_error', ['mean_squared_error'])
 
 history = model.fit_generator(generate_batch_from_data(data_train),
                               samples_per_epoch=32*len(data_train)//32, nb_epoch=5,
+                              #samples_per_epoch=20000, nb_epoch=5,
                               validation_data = generate_batch_from_data(data_val),
                               nb_val_samples = len(data_val),
+                              #nb_val_samples = 2000,
                               verbose = 2)
 
-#with open('model.json', 'w') as json:
-#    json.write(model.to_json())
-#model.save_weights('model.h5')
 model.save('model.h5')
